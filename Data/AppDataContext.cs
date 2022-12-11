bae688c65f3cfb31c45564e747509c8af8ee7235
ziondayoo <adetunjitemidayo26@gmail.com>
2022-12-11 08:20:28 +0100

    Add project files.

---
+++ Data/AppDataContext.cs
@@ -0,0 +1,17 @@
+﻿using ExamManagementAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace ExamManagementAPI.Data
+{
+    public class AppDataContext:DbContext
+    {
+        public AppDataContext(DbContextOptions<AppDataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Exam> Exams { get; set; }
+        public DbSet<TheoryQuestion> TheoryQuestions { get; set; }
+        public DbSet<ObjectiveQuestion> ObjectiveQuestions  { get; set; }
+    }
+}
